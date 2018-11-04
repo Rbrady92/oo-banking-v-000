7f237432379ceca8bd1ca42fc244d1f53ad1097b
Ryan Brady <ryanbrady369@gmail.com>
2018-11-04 22:24:01 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,22 @@
 class BankAccount
+  attr_accessor :name, :balance, :status
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def display_balance
+    @balance
+  end
+
+  def valid?
+    @status == "open" && @balance > 0
+  end
+
+  def close_account
+    @status = "closed"
+  end
 
 end
